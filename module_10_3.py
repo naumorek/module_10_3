b4d96d5f556fdb68a3882180e101d689b59a65d2
naumorek <38781843+naumorek@users.noreply.github.com>
2024-11-26 11:10:11 +0300

    Add files via upload

--- module_10_3.py
+++ module_10_3.py
@@ -79,48 +79,52 @@ import threading
 import random
 import time
 
-lock=threading.Lock()
+lock = threading.Lock()
+
 
 class Bank(threading.Thread):
-    global lock
-    def __init__(self,balance):
+
+
+    def __init__(self, balance):
         threading.Thread.__init__(self)
-        self.balance=balance
-        lock.acquire()
+        self.balance = 0
+        self.lock = threading.Lock()
 
     def deposit(self):
         for i in range(100):
-            rand_int=random.randint(50,500)
+            rand_int = random.randint(50, 500)
 
-            if self.balance>500 and lock.locked():
-                lock.release()
             self.balance = self.balance + rand_int
+
             print(f"Пополнение: {rand_int}. Баланс: {self.balance}")
+            if self.balance > 500 and self.lock.locked():
+                self.lock.release()
             time.sleep(0.001)
 
     def take(self):
+
         for i in range(100):
-            rand_int=random.randint(50,500)
+
+            rand_int = random.randint(50, 500)
             print(f'Запрос на {rand_int}')
-            if rand_int>self.balance:
-                print("Запрос отклонён, недостаточно средств")
-                lock.acquire()
-            else:
-                self.balance=self.balance-rand_int
+            if rand_int <= self.balance:
+                self.balance = self.balance - rand_int
                 print(f"Снятие: {rand_int}. Баланс: {self.balance}")
+
+
+
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                self.lock.acquire()
+
             time.sleep(0.001)
 
 
 bk = Bank(100)
-th1 = threading.Thread(target=bk.deposit)
-th2 = threading.Thread(target=bk.take)
-
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
 th1.start()
 th2.start()
 th1.join()
 th2.join()
 print(f'Итоговый баланс: {bk.balance}')
-
-
-
-
